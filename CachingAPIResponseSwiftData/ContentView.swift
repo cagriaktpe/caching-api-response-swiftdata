80f240a61825ce730dbcdd9203e0e8bdc9d1f48e
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-23 13:42:30 +0300

    Clean up data in SwiftData

--- CachingAPIResponseSwiftData/ContentView.swift
+++ CachingAPIResponseSwiftData/ContentView.swift
@@ -5,16 +5,18 @@
 //  Created by Tunde Adegoroye on 24/07/2023.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    
+
     @AppStorage("lastFetched") private var lastFetched: Double = Date.now.timeIntervalSince1970
-    
+
     @Query(sort: \Photo.id) private var photos: [Photo]
-    
+
+    @State private var isLoading:Bool = false
+
     var body: some View {
         NavigationStack {
             List {
@@ -23,7 +25,7 @@ struct ContentView: View {
                         AsyncImage(url: item.url) { image in
                             image
                                 .resizable()
-                                
+
                                 .frame(maxWidth: .infinity)
                                 .frame(height: 300)
                                 .clipped()
@@ -32,8 +34,7 @@ struct ContentView: View {
                         }
                         .frame(maxWidth: .infinity)
                         .frame(height: 300)
-                        
-                            
+
                         Text(item.title)
                             .font(.caption)
                             .bold()
@@ -43,9 +44,7 @@ struct ContentView: View {
                     .padding(.bottom)
                     .background(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 10,
-                                                style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-                    
-                   
+                                                style: /*@START_MENU_TOKEN@*/ .continuous/*@END_MENU_TOKEN@*/))
                 }
                 .listRowSeparator(.hidden)
                 .listRowBackground(Color.clear)
@@ -56,12 +55,19 @@ struct ContentView: View {
             .background(Color(uiColor: .systemGroupedBackground))
             .task {
                 do {
+                    isLoading = true
+                    defer { isLoading = false }
                     if hasExceedLimit() || photos.isEmpty {
                         print("Fetching data")
                         try await fetchPhotos()
                     }
                 } catch {
                     print(error.localizedDescription)
+                }
+            }
+            .overlay {
+                if isLoading {
+                    ProgressView()
                 }
             }
         }
@@ -77,12 +83,12 @@ struct ContentView: View {
 class Photo: Codable {
     @Attribute(.unique)
     var id: Int?
-    
+
     var albumId: Int
     var title: String
     var url: URL
     var thumbnailUrl: URL
-    
+
     enum CodingKeys: String, CodingKey {
         case id
         case albumId
@@ -90,17 +96,17 @@ class Photo: Codable {
         case url
         case thumbnailUrl
     }
-    
+
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.id = try container.decode(Int?.self, forKey: .id)
-        self.albumId = try container.decode(Int.self, forKey: .albumId)
-        self.title = try container.decode(String.self, forKey: .title)
-        self.url = try container.decode(URL.self, forKey: .url)
-        self.thumbnailUrl = try container.decode(URL.self, forKey: .thumbnailUrl)
+
+        id = try container.decode(Int?.self, forKey: .id)
+        albumId = try container.decode(Int.self, forKey: .albumId)
+        title = try container.decode(String.self, forKey: .title)
+        url = try container.decode(URL.self, forKey: .url)
+        thumbnailUrl = try container.decode(URL.self, forKey: .thumbnailUrl)
     }
-    
+
     func encode(to encoder: Encoder) throws {
         // TODO: Implement
     }
@@ -112,23 +118,28 @@ extension ContentView {
         let request = URLRequest(url: url)
         let (data, _) = try await URLSession.shared.data(for: request)
         let photos = try! JSONDecoder().decode([Photo].self, from: data)
-        
+
         photos.forEach {
+            clearPhotos()
             modelContext.insert($0)
         }
-        
+
         lastFetched = Date.now.timeIntervalSince1970
     }
-    
+
     func hasExceedLimit() -> Bool {
         let timeLimit = 1
         let currentTime = Date.now
         let lastFetchedTime = Date(timeIntervalSince1970: lastFetched)
-        
+
         guard let differenceInMins = Calendar.current.dateComponents([.second], from: lastFetchedTime, to: currentTime).second else {
             return false
         }
-        
+
         return differenceInMins > timeLimit
+    }
+
+    func clearPhotos() {
+        _ = try? modelContext.delete(model: Photo.self)
     }
 }
