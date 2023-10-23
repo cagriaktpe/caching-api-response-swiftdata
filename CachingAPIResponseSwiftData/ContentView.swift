10b0f5d6cfb94cfa113c4c387e39b86739d9bc81
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-10-23 11:30:27 +0300

    Create model and fetchPhotos service

--- CachingAPIResponseSwiftData/ContentView.swift
+++ CachingAPIResponseSwiftData/ContentView.swift
@@ -8,23 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var photos: [Photo] = []
+    
     var body: some View {
         NavigationStack {
             List {
-                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
+                ForEach(photos, id: \.id) { item in
                     VStack(alignment: .leading) {
-                        Rectangle()
-                            .fill(.blue)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 300)
+                        AsyncImage(url: item.url) { image in
+                            image
+                                .resizable()
+                                
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 300)
+                                .clipped()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 300)
+                        
                             
-                        Text("[Title here]")
+                        Text(item.title)
                             .font(.caption)
                             .bold()
                             .padding(.horizontal)
                             .padding(.top)
                     }
-                    
                     .padding(.bottom)
                     .background(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 10,
@@ -39,10 +49,36 @@ struct ContentView: View {
             .listStyle(.plain)
             .scrollContentBackground(.hidden)
             .background(Color(uiColor: .systemGroupedBackground))
+            .task {
+                do {
+                    try await fetchPhotos()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Photo: Codable {
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: URL
+    let thumbnailUrl: URL
+}
+
+extension ContentView {
+    func fetchPhotos() async throws {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/photos")!
+        let request = URLRequest(url: url)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let photos = try! JSONDecoder().decode([Photo].self, from: data)
+        
+        self.photos = photos
+    }
 }
